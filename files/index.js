2571c21096cd248a1680cf5c8b6b1fe6438e4e12
lancer779 <lancer779@disroot.org>
2024-12-01 06:25:38 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
-const port = process.env.SERVER_PORT || process.env.PORT || 5009;        
-const UUID = process.env.UUID || '1fea01a2-e355-4ed9-8170-367bc9f3e7ab'; //若需要改UUID，需要在config.json里改为一致
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
-const NEZHA_PORT = process.env.NEZHA_PORT || ''; 
-const NEZHA_KEY = process.env.NEZHA_KEY || '';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
-const CFIP = process.env.CFIP || 'na.ma';
-const NAME = process.env.NAME || 'Choreo';
+const port = process.env.SERVER_PORT || process.env.PORT || 5009;
+const UUID = process.env.UUID || '1fea01a2-e355-4ed9-8170-367bc9f3e7ab';
+const JINZHA_WSPATH = process.env.JINZHA_WSPATH || '';
+const JINZHA_PORT = process.env.JINZHA_PORT || '';
+const JINZHA_KEY = process.env.JINZHA_KEY || '';
+const TAGO_DOMAIN = process.env.TAGO_DOMAIN || 'chaonone.clisaxlo.eu.org';
+const TAGO_AUTH = process.env.TAGO_AUTH || 'eyJhIjoiMDk0OTIxZDhmY2JmMWI1ODk4NjVmMDhhNDAxYzBhZGUiLCJ0IjoiM2IxNmYxMjQtNzZmYy00ZmE3LWIzZmEtMGIzMDc0YjJmZTQ3IiwicyI6IlltRTJZVGt4WVRVdFptVmtZUzAwTlRRMExUZ3pOMk10T0RNeE16YzFZVEJpWkRsbCJ9';
+const YXFC = process.env.YXFC || 'visa.co.jp';
+const MME = process.env.MME || 'oerohC';
 
 // root route
 app.get("/", function(req, res) {
-  res.send("Free Day!");
+  res.send("Free Happy!");
 });
 
 const metaInfo = execSync(
@@ -23,11 +23,11 @@ const metaInfo = execSync(
 const ISP = metaInfo.trim();
 
 // sub subscription
-app.get('/sub', (req, res) => {
-  const VMESS = { v: '2', ps: `${NAME}-${ISP}`, add: CFIP, port: '8443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: ARGO_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: ARGO_DOMAIN, alpn: '' };
-  const vlessURL = `vless://${UUID}@${CFIP}:8443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fvless?ed=2048#${NAME}-${ISP}`;
+app.get('/scp0', (req, res) => {
+  const VMESS = { v: '2', ps: `${MME}-${ISP}`, add: YXFC, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: TAGO_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: TAGO_DOMAIN, alpn: '' };
+  const vlessURL = `vless://${UUID}@${YXFC}:443?encryption=none&security=tls&sni=${TAGO_DOMAIN}&type=ws&host=${TAGO_DOMAIN}&path=%2Fvless?ed=2048#${MME}-${ISP}`;
   const vmessURL = `vmess://${Buffer.from(JSON.stringify(VMESS)).toString('base64')}`;
-  const trojanURL = `trojan://${UUID}@${CFIP}:8443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Ftrojan?ed=2048#${NAME}-${ISP}`;
+  const trojanURL = `trojan://${UUID}@${YXFC}:443?security=tls&sni=${TAGO_DOMAIN}&type=ws&host=${TAGO_DOMAIN}&path=%2Ftrojan?ed=2048#${MME}-${ISP}`;
   
   const base64Content = Buffer.from(`${vlessURL}\n\n${vmessURL}\n\n${trojanURL}`).toString('base64');
 
@@ -36,14 +36,14 @@ app.get('/sub', (req, res) => {
 
 // run-nezha
   let NEZHA_TLS = '';
-  if (NEZHA_SERVER && NEZHA_PORT && NEZHA_KEY) {
+  if (JINZHA_WSPATH && JINZHA_PORT && JINZHA_KEY) {
     const tlsPorts = ['443', '8443', '2096', '2087', '2083', '2053'];
-    if (tlsPorts.includes(NEZHA_PORT)) {
+    if (tlsPorts.includes(JINZHA_PORT)) {
       NEZHA_TLS = '--tls';
     } else {
       NEZHA_TLS = '';
     }
-  const command = `nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &`;
+  const command = `nohup ./swith -s ${JINZHA_WSPATH}:${JINZHA_PORT} -p ${JINZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &`;
   try {
     exec(command);
     console.log('swith is running');
@@ -78,7 +78,7 @@ function runWeb() {
 // run-server
 function runServer() {
   let command2 = '';
-  if (ARGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
+  if (TAGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
     command2 = `nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH} >/dev/null 2>&1 &`;
   } else {
     command2 = `nohup ./server tunnel --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &`;

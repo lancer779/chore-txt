2e42406293e9155e0ce7e778ac94c59f979ab055
lancer779 <lancer779@disroot.org>
2024-12-04 00:35:25 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '1fea01a2-e355-4ed9-8170-367bc9f3e7ab';
 const JINZHA_WSPATH = process.env.JINZHA_WSPATH || '';
 const JINZHA_PORT = process.env.JINZHA_PORT || '';
 const JINZHA_KEY = process.env.JINZHA_KEY || '';
-const TAGO_DOMAIN = process.env.TAGO_DOMAIN || 'chaonone.clisaxlo.eu.org';
-const TAGO_AUTH = process.env.TAGO_AUTH || 'eyJhIjoiMDk0OTIxZDhmY2JmMWI1ODk4NjVmMDhhNDAxYzBhZGUiLCJ0IjoiM2IxNmYxMjQtNzZmYy00ZmE3LWIzZmEtMGIzMDc0YjJmZTQ3IiwicyI6IlltRTJZVGt4WVRVdFptVmtZUzAwTlRRMExUZ3pOMk10T0RNeE16YzFZVEJpWkRsbCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chaonone.clisaxlo.eu.org';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMDk0OTIxZDhmY2JmMWI1ODk4NjVmMDhhNDAxYzBhZGUiLCJ0IjoiM2IxNmYxMjQtNzZmYy00ZmE3LWIzZmEtMGIzMDc0YjJmZTQ3IiwicyI6IlltRTJZVGt4WVRVdFptVmtZUzAwTlRRMExUZ3pOMk10T0RNeE16YzFZVEJpWkRsbCJ9';
 const YXFC = process.env.YXFC || 'visa.co.jp';
 const MME = process.env.MME || 'oerohC';
 
@@ -23,11 +23,11 @@ const metaInfo = execSync(
 const ISP = metaInfo.trim();
 
 // sub subscription
-app.get('/scp0', (req, res) => {
-  const VMESS = { v: '2', ps: `${MME}-${ISP}`, add: YXFC, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: TAGO_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: TAGO_DOMAIN, alpn: '' };
-  const vlessURL = `vless://${UUID}@${YXFC}:443?encryption=none&security=tls&sni=${TAGO_DOMAIN}&type=ws&host=${TAGO_DOMAIN}&path=%2Fvless?ed=2048#${MME}-${ISP}`;
+app.get('/sub0', (req, res) => {
+  const VMESS = { v: '2', ps: `${MME}-${ISP}`, add: YXFC, port: '443', id: UUID, aid: '0', scy: 'none', net: 'ws', type: 'none', host: ARGO_DOMAIN, path: '/vmess?ed=2048', tls: 'tls', sni: ARGO_DOMAIN, alpn: '' };
+  const vlessURL = `vless://${UUID}@${YXFC}:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fvless?ed=2048#${MME}-${ISP}`;
   const vmessURL = `vmess://${Buffer.from(JSON.stringify(VMESS)).toString('base64')}`;
-  const trojanURL = `trojan://${UUID}@${YXFC}:443?security=tls&sni=${TAGO_DOMAIN}&type=ws&host=${TAGO_DOMAIN}&path=%2Ftrojan?ed=2048#${MME}-${ISP}`;
+  const trojanURL = `trojan://${UUID}@${YXFC}:443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Ftrojan?ed=2048#${MME}-${ISP}`;
   
   const base64Content = Buffer.from(`${vlessURL}\n\n${vmessURL}\n\n${trojanURL}`).toString('base64');
 
@@ -78,7 +78,7 @@ function runWeb() {
 // run-server
 function runServer() {
   let command2 = '';
-  if (TAGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
+  if (ARGO_AUTH.match(/^[A-Z0-9a-z=]{120,250}$/)) {
     command2 = `nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH} >/dev/null 2>&1 &`;
   } else {
     command2 = `nohup ./server tunnel --edge-ip-version auto --config tunnel.yml run >/dev/null 2>&1 &`;
